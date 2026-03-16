1c0455b39935ee49ac06992285cf3ae71ab2ba06
BLadner34 <brittnee.hallman@yahoo.com>
2026-03-16 11:23:16 -0500

    added render function

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
 
 let rickAndMortyId = 1; 
+
+function render(rick){
+    const id = document.createElement("p");
+    const name = document.createElement("p");
+    const species = document.createElement("p");
+    const origin = document.createElement("p");
+    const img = document.createElement("img");
+
+    id.textContent = rick.id
+    name.textContent = rick.name
+    species.textContent = rick.species
+    origin.textContent = rick.origin.name
+    img.src = rick.image
+    img.alt = rick.name
+
+    output.textContent = ""
+    output.appendChild(id);
+    output.appendChild(name);
+    output.appendChild(species);
+    output.appendChild(origin);
+    output.appendChild(img);
+}
 //Function for API
 async function getInformation(url){
     try {
@@ -28,16 +50,20 @@ async function main(){
           "https://rickandmortyapi.com/api/character/" + rickAndMortyId);
           console.log(rick)
         prev.addEventListener("click", async () => {
+           if(rickAndMortyId < 2){
+            return
+           } rickAndMortyId--
           const rick = await getInformation(
             "https://rickandmortyapi.com/api/character/" + rickAndMortyId);
             console.log(rick);
+            render(rick)
         });
         next.addEventListener("click", async () => {
             rickAndMortyId++
           const rick = await getInformation(
-            "https://rickandmortyapi.com/api/character/" + rickAndMortyId
-          );
-          console.log(rick);
+            "https://rickandmortyapi.com/api/character/" + rickAndMortyId);
+            console.log(rick);
+            render(rick)
         });
     } catch (error) {
         console.error(error)
